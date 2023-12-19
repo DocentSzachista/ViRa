8801207dbcf7cbd798d8ec1795e837cc7dfa6021
Józek <bossowski92@gmail.com>
2023-12-19 20:23:58 +0100

    grabbable notes in grid

--- ViRa2/Assets/NotesGenerator.cs
+++ ViRa2/Assets/NotesGenerator.cs
@@ -2,16 +2,33 @@ using Assets.Scripts.Models.Issues;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
-public class NotesGenerator : MonoBehaviour
+public class NotesManager : MonoBehaviour
 {
     ApiRequest apiRequest;
-    public GameObject BaseNote;
+
+    public GameObject NotePrefab;
+
+    public Transform ToDoSection;
+    public Transform DoingSection;
+    public Transform DoneSection;
+
+    private List<GameObject> ToDoNotes;
+    private List<GameObject> DoingNotes;
+    private List<GameObject> DoneNotes;
+
+    public float offset = 0.05f;
     // Start is called before the first frame update
     void Start()
     {
         apiRequest = new ApiRequest();
+
+        ToDoNotes = new List<GameObject>();
+        DoingNotes = new List<GameObject>();
+        DoneNotes = new List<GameObject>();
 
         StartCoroutine(apiRequest.GetAllIssues(FillBoard));
     }
@@ -20,28 +37,84 @@ public class NotesGenerator : MonoBehaviour
     {
         foreach (var issue in apiRequest.issues)
         {
-            CreateNote(issue);
+            var note = CreateNote(issue);
+            if (note != null)
+            {
+                switch (issue.transitionName)
+                {
+                    case "To Do":
+                        ToDoNotes.Add(note);
+                        break;
+                    case "In Progress":
+                        DoingNotes.Add(note);
+                        break;
+                    case "Done":
+                        DoneNotes.Add(note);
+                        break;
+                }
+            }
+        }
+
+        PlaceNotes(ToDoNotes, ToDoSection);
+        PlaceNotes(DoingNotes, DoingSection);
+        PlaceNotes(DoneNotes, DoneSection);
+    }
+
+    private void PlaceNotes(List<GameObject> notes, Transform section, int startIndex = 0)
+    {
+        var sectionSize = section.GetComponent<BoxCollider>().size;
+        var noteSize = NotePrefab.GetComponent<BoxCollider>().size;
+
+        //Debug.Log($"section size: {sectionSize}");
+        //Debug.Log($"note size: {noteSize}");
+
+        //Debug.Log(sectionSize.x / (noteSize.x * (1 + offset)));
+        int maxColumns = (int)(sectionSize.x / (noteSize.x * (1 + offset)));
+        int maxRows = (int)(sectionSize.y / (noteSize.y * (1 + offset)));
+
+        Vector3 firstPosition = new Vector3(
+            section.transform.position.x - sectionSize.x / 2f + noteSize.x / 2f,
+            section.transform.position.y + sectionSize.y / 2f - noteSize.y / 2f,
+            section.transform.position.z
+            );
+        Debug.Log($"first position: {firstPosition}");
+
+        for (int i = startIndex; i < notes.Count; i++)
+        {
+            int col = i % maxColumns;
+            int row = i / maxColumns;
+            Vector3 newPosition = firstPosition + new Vector3(
+                    col * (noteSize.x * (1 + offset)),
+                    - row * (noteSize.y * (1 + offset)),
+                    0
+                );
+            var postitScript = notes[i].GetComponent<PostItNote>();
+            postitScript.MoveToPosition(newPosition);
         }
     }
 
-    private void CreateNote(Issue issue)
+    private GameObject CreateNote(Issue issue)
     {
+        GameObject newNote = null;
         // Check if the object to copy is not null
-        if (BaseNote != null)
+        if (NotePrefab != null)
         {
             // Instantiate a copy of the object at the same position and rotation
-            GameObject copy = Instantiate(BaseNote, transform.position, BaseNote.transform.rotation);
+            newNote = Instantiate(NotePrefab, transform.position, ToDoSection.rotation);
+            
             PostItNote noteComponent;
-            var result = copy.TryGetComponent(out noteComponent);
+            var result = newNote.TryGetComponent(out noteComponent);
             if (result)
             {
+                newNote.name = "Note-" + issue.id;
+                
                 noteComponent.Description = issue.summary;
                 noteComponent.TaskId = issue.id;
-                copy.name = "Note-" + issue.id;
-                noteComponent.IsTemplate = false;
-                noteComponent.CurrentSection = issue.transitionName;
-                Debug.Log($"Section: {noteComponent.CurrentSection}");
-                noteComponent.MoveToSection();
+                noteComponent.CurrentSectionName = issue.transitionName;
+
+                noteComponent.notesManager = this;
+
+                Debug.Log($"Section: {noteComponent.CurrentSectionName}");
             }
 
         }
@@ -49,6 +122,7 @@ public class NotesGenerator : MonoBehaviour
         {
             Debug.LogWarning("Object to copy is not assigned!");
         }
+        return newNote;
     }
 
     // Update is called once per frame
--- ViRa2/Assets/Scripts/PostItNote.cs
+++ ViRa2/Assets/Scripts/PostItNote.cs
@@ -7,22 +7,18 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class PostItNote : MonoBehaviour
 {
-    public bool IsTemplate;
+    public NotesManager notesManager;
 
-    public string Description;
-    public string CurrentSection;
+    public string Description { get; set; }
+    public string CurrentSectionName;
     public string TaskId;
 
     private string CollidedSection;
 
-    public Transform ToDoSection;
-    public Transform DoingSection;
-    public Transform DoneSection;
-
     public TextMeshProUGUI descriptionText;
 
-
     XRGrabInteractable m_GrabInteractable;
+
     private void Awake()
     {
         m_GrabInteractable = GetComponent<XRGrabInteractable>();
@@ -34,39 +30,21 @@ public class PostItNote : MonoBehaviour
     void Start()
     {
         descriptionText.text = Description;
-        if (!IsTemplate)
+        if (CurrentSectionName.IsNullOrEmpty())
         {
-            if (CurrentSection.IsNullOrEmpty())
-            {
-                CurrentSection = "To Do";
-            }
-            MoveToSection(CurrentSection);
+            CurrentSectionName = "To Do";
         }
     }
 
     private void OnEnable()
     {
         m_GrabInteractable.selectExited.AddListener(OnSelectExit);
-        //m_GrabInteractable.focusEntered.AddListener(OnFocusEntered);
-        //m_GrabInteractable.focusExited.AddListener(OnFocusExited);
     }
 
     private void OnDisable()
     {
         m_GrabInteractable.selectExited.RemoveListener(OnSelectExit);
-        //m_GrabInteractable.focusEntered.RemoveListener(OnFocusEntered);
-        //m_GrabInteractable.focusExited.RemoveListener(OnFocusExited);
     }
-    //private void OnFocusEntered(FocusEnterEventArgs e)
-    //{
-    //    isFocused = true;
-    //}
-
-    //private void OnFocusExited(FocusExitEventArgs e)
-    //{
-    //    isFocused = false;
-    //}
-
     private void OnSelectExit(SelectExitEventArgs arg0) => Invoke(nameof(Dropped), 0.1f);
 
     // Update is called once per frame
@@ -79,40 +57,8 @@ public class PostItNote : MonoBehaviour
     {
         if (!CollidedSection.IsNullOrEmpty())
         {
-            CurrentSection = CollidedSection;
+            CurrentSectionName = CollidedSection;
         }
-        MoveToSection(CurrentSection);
-    }
-
-    public void MoveToSection()
-    {
-        MoveToSection(CurrentSection);
-    }
-
-    void MoveToSection(string section)
-    {
-        Vector3 targetPosition = Vector3.zero;
-        Debug.Log($"Moving to section {section}");
-        switch (section)
-        {
-            case "To Do":
-                targetPosition = ToDoSection.position;
-                targetPosition += GetRandomPositionOffset(ToDoSection.GetComponent<BoxCollider>().size);
-                transform.rotation = ToDoSection.rotation;
-                break;
-            case "In Progress":
-                targetPosition = DoingSection.position;
-                targetPosition += GetRandomPositionOffset(DoingSection.GetComponent<BoxCollider>().size);
-                transform.rotation = DoingSection.rotation;
-                break;
-            case "Done":
-                targetPosition = DoneSection.position;
-                targetPosition += GetRandomPositionOffset(DoneSection.GetComponent<BoxCollider>().size);
-                transform.rotation = DoneSection.rotation;
-                break;
-        }
-
-        StartCoroutine(MoveToPosition(targetPosition, 0.4f)); // Adjust the duration as needed
     }
 
     Vector3 GetRandomPositionOffset(Vector3 sectionSize)
@@ -121,6 +67,11 @@ public class PostItNote : MonoBehaviour
             UnityEngine.Random.Range(-sectionSize.x / 2, sectionSize.x / 2),
             UnityEngine.Random.Range(-sectionSize.y / 2, sectionSize.y / 2),
             0);
+    }
+
+    public void MoveToPosition(Vector3 position)
+    {
+        StartCoroutine(MoveToPosition(position, 0.4f));
     }
 
     IEnumerator MoveToPosition(Vector3 targetPosition, float duration)
